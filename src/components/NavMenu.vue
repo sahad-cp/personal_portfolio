410c916c9c8cedb4a57983e5f3febb179372617e
sahad-cp <sahadmanu4@gmail.com>
2023-12-07 16:57:44 +0530

    sidebar almost finish

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,25 +1,90 @@
 <script setup>
+import { onMounted, ref,h } from "vue";
+import { UnorderedListOutlined,RightOutlined } from "@ant-design/icons-vue"
+
+const mainNav = ref(false)
+const sidebar = ref(false)
+const items = [
+  {
+    key: 'home',
+    label: 'Home',
+    title: 'Home',
+    icon: () => h(RightOutlined ),
+    path: '/#homesection',
+  },
+  {
+    key: 'about',
+    label: 'About',
+    title: 'About',
+    icon: () => h(RightOutlined),
+    path: '/#about_section',
+  },
+  {
+    key: 'skill',
+    label: 'Skill',
+    title: 'Skill',
+    icon: () => h(RightOutlined),
+    path: '/#skill_section',
+  },
+  {
+    key: 'project',
+    label: 'Project',
+    title: 'Project',
+    icon: () => h(RightOutlined),
+    path: '/#project_section',
+  },
+  {
+    key: 'contact',
+    label: 'Contact',
+    title: 'Contact',
+    icon: () => h(RightOutlined),
+    path: '/#contact_section',
+  },
+];
+function checkScreenSize() {
+  if (window.innerWidth < 768) {
+    mainNav.value = true;
+  }
+}
+
+onMounted(() => {
+  checkScreenSize();
+  window.addEventListener("resize", checkScreenSize);
+});
+const showNav = ()=>{
+    sidebar.value = true
+}
+const closeNav = ()=>{
+    sidebar.value = false
+}
+const toggleMenu = () => {
+    sidebar.value = !sidebar.value;
+};
 </script>
 <template>
-    <div class="w-full h-20 flex justify-center items-center shadow-md">
+    <div v-if="!mainNav" class="w-full h-20 flex justify-center items-center shadow-md" id="nav-main-div">
         <nav class="flex space-x-8">
-            <li class="font-bold hover:text-orange-600">
+            <li class="font-bold hover:text-orange-600 text-white">
                 <a href="#homesection">Home</a>
             </li>
-            <li class="font-bold hover:text-orange-600">
+            <li class="font-bold hover:text-orange-600 text-white">
                 <a href="#about_section">About</a>
             </li>
-            <li class="font-bold hover:text-orange-600">
+            <li class="font-bold hover:text-orange-600 text-white">
                 <a href="#skill_section">Skill</a>
             </li>
-            <li class="font-bold hover:text-orange-600">
+            <li class="font-bold hover:text-orange-600 text-white">
                 <a href="#project_section"> Project</a>
             </li>
-            <li class="font-bold hover:text-orange-600">
+            <li class="font-bold hover:text-orange-600 text-white">
                 <a href="#contact_section">Contact</a>
             </li>
         </nav>
     </div>
+    <div v-else class="w-full bg-white h-10 flex justify-end pr-2 items-center" id="second-div-nav">
+        <UnorderedListOutlined class="text-white w-5 text-2xl mr-3" @click="toggleMenu"/>
+    </div>
+    <a-menu v-if="sidebar" :open="sidebar" :items="items" mode="inline" left :style="{ left: sidebar ? 0 : '-100%' }" />
 </template>
 
 <style  scoped>
@@ -29,5 +94,10 @@ li{
 
 li .active-link{
     border-bottom: 3px solid rgb(255, 106, 0);
+}
+#nav-main-div,#second-div-nav{
+    background-image: linear-gradient(360deg,rgb(0,0,0,0.8),rgb(0,0,0,0.8)),url('../assets/nav-bg.jpeg');
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 </style> 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Antd from 'ant-design-vue';
 
 const app = createApp(App)
-
+app.use(Antd)
 app.use(router)
 
 app.mount('#app')
